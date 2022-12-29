7146cd6945433310e3ad2ac6ffb42b1441391f37
sword-ha <119216976+sword-ha@users.noreply.github.com>
2022-12-29 17:29:06 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,i, sum;
+            Console.WriteLine("enter the first number");
+            int mn = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second number");
+            int mx = int.Parse(Console.ReadLine());
+
+            for(n=mn; n<=mx;n++)
+            {
+                i = 1;  sum=0;
+                while(i<n)
+                {
+                    if (n%i==0)
+                        sum += i;
+                        i++;
+                }
+                if (sum == n)
+                    Console.WriteLine(n);
+            }
+            Console.WriteLine(" ");
+
+        }
+       
+    }
+}
---
+++ dr samir prime/dr samir prime/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dr_samir_prime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("please enter the first number");
+            int firstnumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter the second number");
+            int secondnumber = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            
+            for (int i =firstnumber ; i <= secondnumber; i++)
+            {
+                for (int x = 1; x <= i; x++)
+                {
+                    if (i % x == 0)
+                        sum++;
+                }
+                if (sum == 2)
+                {
+                    Console.WriteLine(i);
+                    sum = 0;
+                }
+                else
+                {
+                    sum = 0;
+                    
+                        }
+            }
+        }
+    }
+}
